768e514e65a5e5a072c862f2aef944617b384d81
diarmaidm <diarmaidm@users.noreply.github.com>
2017-06-20 00:54:40 +0100

    add need Card skeleton Component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = (props) => {
+  return (
+    <div>
+      <img src="http://placehold.it/75" alt="card"/>
+      <div>
+        <div>Name here...</div>
+        <div>Company Name here...</div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
