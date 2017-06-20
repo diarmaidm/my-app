5a56307d746b4b1efc3470edc3d50b36f3c4f25b
diarmaidm <diarmaidm@users.noreply.github.com>
2017-06-20 23:49:41 +0100

    cleanup

--- src/CardList.js
+++ src/CardList.js
@@ -3,7 +3,6 @@ import Card from './Card';
 
 const CardList = (props) => (
     <div>
-      {/*{props.cards.map(card => <Card name={card.name} avatar_url={card.avatar_url} company={card.company} />)}*/}
       {props.cards.map(card => <Card {...card} />)}
     </div>
   )
