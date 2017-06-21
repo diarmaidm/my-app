c7a8b867d58bd7c29bcfa49c43f7b3e0f5fe7762
diarmaidm <diarmaidm@users.noreply.github.com>
2017-06-22 00:37:51 +0100

    added key to data to resolve warning

--- src/App.js
+++ src/App.js
@@ -34,10 +34,13 @@ class App extends Component {
     counter: 0,
     cards: [
       {
+        key: 1,
         name: "Paul O’Shannessy",
         avatar_url: "https://avatars.githubusercontent.com/u/8445?v=3",
         company: "Facebook"
-      }, {
+      },
+      {
+        key: 2,
         name: "Ben Alpert",
         avatar_url: "https://avatars.githubusercontent.com/u/6820?v=3",
         company: "Facebook"
