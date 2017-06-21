ab653e67487873de855c3091b57c4a1b47691718
diarmaidm <diarmaidm@users.noreply.github.com>
2017-06-22 00:36:51 +0100

    add skeleton event handler

--- src/Form.js
+++ src/Form.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 class Form extends React.Component {
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Event: Form Submit');
+  }
   render() {
     return (
-      <form>
+      <form onSubmit={ this.handleSubmit }>
         <input type="text" placeholder="Github username" />
         <button type="submit">Add card</button>
       </form>
