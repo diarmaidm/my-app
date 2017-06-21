9758b086b54a2d4b3842371d9d345ae0dd29a955
diarmaidm <diarmaidm@users.noreply.github.com>
2017-06-21 23:19:35 +0100

    Add new form component

--- src/CardList.js
+++ src/CardList.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Card from './Card';
+import Form from './Form';
 
 const CardList = (props) => (
-    <div>
-      {props.cards.map(card => <Card {...card} />)}
-    </div>
-  )
+  <div>
+    <Form/> {props
+      .cards
+      .map(card => <Card {...card}/>)}
+  </div>
+)
 
 export default CardList;
